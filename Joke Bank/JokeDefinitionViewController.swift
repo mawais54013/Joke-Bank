54a066b96d73c8f2b77c510e0a6577f0714bd1ab
MAwais <mawais54013@outlook.com>
2019-09-29 23:10:23 -0700

    Added Jokes And Finished

--- Joke Bank/JokeDefinitionViewController.swift
+++ Joke Bank/JokeDefinitionViewController.swift
@@ -11,12 +11,30 @@ import UIKit
 class JokeDefinitionViewController: UIViewController {
     
     var joke = ""
-
+    @IBOutlet weak var jokeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = joke
-        // Do any additional setup after loading the view.
+        
+//        "Chicken", "Walk into a Bar...", "Olive", "Racer"
+        if joke == "Chicken"
+        {
+            jokeLabel.text = "Q: Why did the chicken cross the road?\n\nA: To get to the other side"
+        }
+        else if joke == "Walk into a Bar..."
+        {
+            jokeLabel.text = "A horse walks into a bar and the bartender says 'Why the long face'"
+        }
+        else if joke == "Olive"
+        {
+            jokeLabel.text = "Two olives are on an olive tree branck playing around. One of the olives bumps the other and knocks him off the branch. His friend wobbles at the edge and shouts down, 'Are you ok!'\n\nA: Hi friend on the squeaks out, 'Don't worry..olive...'"
+        }
+        else if joke == "Racer"
+        {
+            jokeLabel.text = "Q: What did the hot dog say when he crossed the finish line?\n\nA: I'm the weiner!"
+        }
     }
 
 }
